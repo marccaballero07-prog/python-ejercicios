9c7001e035200e4bfd14a07d1c0b684ad4cd0316
marccaballero07-prog <marccaballero07@gmail.com>
2026-05-05 11:29:41 +0200

    Update ejercicio1.py
    Ejercicio 1 completado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,7 @@
-print("Hola Marc, primer commit")
+num1 = int(input("Introduce el primer número: "))
+num2 = int(input("Introduce el segundo número: "))
+
+resultado = num1 + num2
+
+print("El resultado es:", resultado)
+
