d589a52237d7aa13d5e67657b341fe526abcc388
marccaballero07-prog <marccaballero07@gmail.com>
2026-05-05 16:48:59 +0200

    Ejercicio11 completado
    Detección de mayúscula, minúscula o número usando isupper, islower o isnumeric.

---
+++ ejercicio11.py
@@ -0,0 +1,10 @@
+caracter = input("Introduce un carácter: ")
+
+if caracter.isupper():
+    print("La letra es mayúscula")
+
+if caracter.islower():
+    print("La letra es minúscula")
+
+if caracter.isnumeric():
+    print("El valor introducido es un número")
