36b526ff830ac91438165ca59496b13be95f43af
marccaballero07-prog <marccaballero07@gmail.com>
2026-05-05 15:46:14 +0200

    ejercicio 5 completado

---
+++ ejercicio5.py
@@ -0,0 +1,9 @@
+import math
+
+num = float(input("Introduce un número: "))
+
+raiz = math.sqrt(num)
+division = raiz / 2
+
+print("El resultado de la raíz es:", raiz)
+print("El resultado de la división es:", division)
