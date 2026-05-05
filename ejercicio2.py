786c09d369c1d822382a057239900a1581cde0bd
marccaballero07-prog <marccaballero07@gmail.com>
2026-05-05 13:08:20 +0200

    Ejercicio 2 completado
    Ejercicio 2 usando float (decimales)

---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+num1 = float(input("Introduce el primer número (decimal): "))
+num2 = float(input("Introduce el segundo número (decimal): "))
+
+resultado = num1 + num2
+
+print("El resultado es:", resultado)
